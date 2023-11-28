26c47a30b4eadecf0d918c39662b2b89c6c7a8eb
Frank <40611willy@gmail.com>
2023-11-28 16:49:52 +0800

    chore: Remove react

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -5,8 +5,8 @@ const srcDir = path.join(__dirname, "..", "src");
 
 module.exports = {
     entry: {
-        popup: path.join(srcDir, 'popup.tsx'),
         content_script: path.join(srcDir, 'content_script.ts'),
+        injected: path.join(srcDir, 'injected.js'),
     },
     output: {
         path: path.join(__dirname, "../dist/js"),
@@ -16,7 +16,7 @@ module.exports = {
         splitChunks: {
             name: "vendor",
             chunks(chunk) {
-                return chunk.name !== 'background';
+                return chunk.name !== 'injected';
             }
         },
     },
