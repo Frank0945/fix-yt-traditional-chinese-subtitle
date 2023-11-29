bc6be62b39bdb015decceccfe2d955069b66ca14
Frank <40611willy@gmail.com>
2023-11-30 02:41:05 +0800

    chore: Prettier in webpack

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -4,38 +4,38 @@ const CopyPlugin = require("copy-webpack-plugin");
 const srcDir = path.join(__dirname, "..", "src");
 
 module.exports = {
-    entry: {
-        content_script: path.join(srcDir, 'content_script.ts'),
-        injected: path.join(srcDir, 'injected.js'),
+  entry: {
+    content_script: path.join(srcDir, "content_script.ts"),
+    injected: path.join(srcDir, "injected.ts"),
+  },
+  output: {
+    path: path.join(__dirname, "../dist/js"),
+    filename: "[name].js",
+  },
+  optimization: {
+    splitChunks: {
+      name: "vendor",
+      chunks(chunk) {
+        return chunk.name !== "injected";
+      },
     },
-    output: {
-        path: path.join(__dirname, "../dist/js"),
-        filename: "[name].js",
-    },
-    optimization: {
-        splitChunks: {
-            name: "vendor",
-            chunks(chunk) {
-                return chunk.name !== 'injected';
-            }
-        },
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [{ from: ".", to: "../", context: "public" }],
-            options: {},
-        }),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
     ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: ".", to: "../", context: "public" }],
+      options: {},
+    }),
+  ],
 };
